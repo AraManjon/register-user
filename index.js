09467bc5982aabcde16c66ba8dd646c5395009a5
Andrea Trussardi <andreatrussardi@imac-di-andrea-trussardi.local>
2022-05-03 17:26:05 +0200

    Utilizando innerHTML elimnando alert

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const buttonHTML = document.getElementById("addUser");
 buttonHTML.addEventListener("click", addUser); 
 
 const usuariosArray = []
+
 function addUser() {
 const userName = document.getElementById("userName").value
 const password = document.getElementById("password").value
@@ -14,15 +15,16 @@ console.log(usuariosArray)
 
 const button1HTML = document.getElementById("logIn");
 button1HTML.addEventListener("click", logIn); 
+const result = document.getElementById("result")
 
 function logIn() {
     const verifyUsername = document.getElementById("loginUsername").value
     const verifyPassword = document.getElementById("loginPassword").value
     const userFound = usuariosArray.find( user => user.UserName === verifyUsername && user.Password === verifyPassword)
     if (userFound) {
-        alert('El usuario existe')
+        result.innerHTML = 'El usuario existe'
     }
     if(!userFound) {
-        alert('El usuario no existe')
+        result.innerHTML = 'El usuario NO existe'
     }
 }
